97d43d49a595848428899b1e61f94d3770581fbe
Ross LeBeau <ross.lebeau@gmail.com>
2015-06-23 15:22:43 -0400

    Initial commit, all files present and working

---
+++ Source/EPCalloutDefaultContentView.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+@interface EPCalloutViewDefaultContentView : UIView
+@property (strong, nonatomic) UILabel *titleLabel;
+@property (strong, nonatomic) UILabel *subtitleLabel;
+@property (strong, nonatomic) UIImageView *accessoryView;
+@end
